063463fdb9ed37d6696642005b14a35c6b3eea9f
DZHURA0o0 <maltsev14022005@gmail.com>
2023-12-03 19:15:47 +0200

    ew
    ew

--- CursachFront.Core/obj/Debug/net7.0-windows/CursachFront.Core.AssemblyInfo.cs
+++ CursachFront.Core/obj/Debug/net7.0-windows/CursachFront.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CursachFront.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3a8619a173ce3e3f38751ba7f82a7633a870ea3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CursachFront.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CursachFront.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
